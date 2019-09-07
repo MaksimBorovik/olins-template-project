7dd52bd36e4160cc6677d6e258dd5936a7b2a89f
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-07 04:54:30 +0300

    second section is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     autoplay: true,
     dots: true
   });
+
+  $('.main-features__team-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScrol: 1,
+    appendArrows: '.main-features__team-buttons',
+    prevArrow: '<button type="button" class="main-features__team-buttons-prev"></button>',
+    nextArrow: '<button type="button" class="main-features__team-buttons-next"></button>'
+  });
 });
