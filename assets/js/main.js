9355b1ffbbf68beec72d8da7af3cd209bef85851
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-10 23:33:31 +0300

    added an adaptive style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.header-slider').slick({
-    autoplay: true,
+    // autoplay: true,
     dots: true
   });
 
@@ -10,6 +10,48 @@ $(document).ready(function(){
     slidesToScrol: 1,
     appendArrows: '.main-features__team-buttons',
     prevArrow: '<button type="button" class="main-features__team-buttons-prev"></button>',
-    nextArrow: '<button type="button" class="main-features__team-buttons-next"></button>'
+    nextArrow: '<button type="button" class="main-features__team-buttons-next"></button>',
+    responsive: [
+      {
+        breakpoint: 1401,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 1130,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 677,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 531,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+      // {
+      //   breakpoint: 480,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1
+      //   }
+      // }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
